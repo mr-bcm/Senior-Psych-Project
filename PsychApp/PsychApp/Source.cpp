e532931acfbd6d97129c527a81495c1b199113fc
Brennan <bmetzelaar16@my.whitworth.edu>
2014-08-27 17:13:23 -0700

    "updated source.cpp"

--- PsychApp/PsychApp/Source.cpp
+++ PsychApp/PsychApp/Source.cpp
@@ -1,71 +1,17 @@
-// Project by Emma Gebben
-// Started July 15, 2014
-// Edited by Brennan Metzelaar & Timothy Bradford
+// Data Compiler: to take information from an EEG .csv file, store certain parts of the data
+// in the program, catalogue those data points and find percentage of each in 1, 5, and 10 minute increments.
+
+// Emma Gebben
+// July 15, 2014 [1st attempt at build, with outline from Pete Tucker: CS Professor at Whitworth University]
 
 #include <iostream>
-#include <string>// so I can have strings
-#include <fstream>// for file inpu and output
-#include <ctime>	// for random number generating
-#include <cstdlib>	// for random number generating
-#include <vector>	// allows us to use a vector
+#include <string>	//bcm
+#include <fstream>
+#include <vector>
+
+
 using namespace std;
 
-// function prototype. This is needed when a function comes after main(){}. It just lets main know that something may come up that requires it to look outside itself.
-vector<string> split(string);
-
-int main(){
-
-	string line; // line is a string that is used to get parts of the .csv file for us so that we can then store it in the vector
-	vector<float> alldata; // vector alldata will be used to store all data from the .csv file
-	int count = 0;	// NEEDS COMMENT
-	float sum = 0;	// NEEDS COMMENT
-
-	// Open the file 2301data.csv (for writing only)
-	ifstream inputer("2301data.csv",ios::in);
-	
-	// checking to see if the program has been opened or not
-	if(!inputer){
-		cout<<"failed badly\n";
-		return 0;
-	}
-	else if(inputer){cout << "it opened!!!\n";}
-
-	//// this copys 2301data.csv into my doc string
-	//while(getline(inputer,doc))
-	//{	
-
-	//	cout << doc <<endl;
-	//}
-
-
-	// Going to read the document into a vector
-	while(getline(inputer,line)) // inputer is the ifstream that is dealing with the .csv file
-	{	
-		// this was made by Pete Tucker
-		vector<string> row = split(line);
-		alldata.push_back(atof(row[1].c_str()));	// Debug: this is where we crash. Specifically the issue is with "atof(row[1].c_str())"
-		// atof is used to convert string to double.
-		
-		// cout << line <<endl; // was used to output the .csv file contents.
-	}
-
-
-   inputer.close();// closing the text doc
-
-   // made by Pete Tucker
-   for (int i = 0; i < alldata.size(); i++) {
-	   count++;
-	   sum += alldata[i];
-   }
-
-   // made by Pete Tucker
-   cout << "average: " << sum / count << "\n";
-
-	cout << line << "this is where line(doc) is \n"; // outputs my doc string
-	
-}
-
-// function made by Pete Tucker
 vector<string> split(string s) {
 	vector<string> ret;
 	size_t start = 0;
@@ -78,3 +24,68 @@ vector<string> split(string s) {
 
 	return ret;
 }
+
+
+int main() {
+
+
+	string line;
+	int count = 0;
+	float sum = 0;
+	vector<float> alldata;
+
+
+	ifstream data; // ifstream (input file stream) because we are reading from a file
+	data.open("2301data.csv"); // Open the .csv file (p430 talks more about different file modes)
+
+	// So we are checking to see if the file exists and can be opened (p425).
+	if (data.fail()) {
+		cout << "File does not exist." << endl;
+		cout << "Exit program." << endl;
+		exit(1);	// We exit the program
+	}
+	else if (!data.fail()) {				// If the file did not fail. This isn't necessary, I just do it to write a message that lets us know its happy.
+		cout << "CSV file opened!\n";		// The message, its happy! :)
+	}
+
+	// copy the file 2301data.csv into the 'line' string.
+	while (getline(data, line)) {
+		cout << line << endl;
+	}
+
+	data.close();	// close the .csv file. We always want to do this when we are done writing or reading from the file in our program.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//// was: while(data.getline(line)) { error
+	//while (getline(data, line)) {
+	//	vector<string> row = split(line);
+	//	alldata.push_back(atof(row[1].c_str()));
+	//}
+
+	//for (int i = 0; i < alldata.size(); i++) {
+	//	count++;
+	//	sum += alldata[i];
+	//}
+
+	//cout << "average: " << sum / count << "\n";
+}
+
